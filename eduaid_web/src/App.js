865707b5196e7d688d4bce86f050734ab197d99d
tirth707 <ptirth2206@gmail.com>
2026-02-25 13:34:20 +0530

    perf: implement route-based code splitting to optimize initial bundle size

--- eduaid_web/src/App.js
+++ eduaid_web/src/App.js
@@ -1,23 +1,33 @@
+import React, { Suspense, lazy } from "react";
 import "./App.css";
 import { Routes, Route, HashRouter } from "react-router-dom";
-import Home from "./pages/Home";
-import Question_Type from "./pages/Question_Type";
-import Text_Input from "./pages/Text_Input";
-import Output from "./pages/Output";
-import Previous from "./pages/Previous";
-import NotFound from "./pages/PageNotFound";
+
+const Home = lazy(() => import("./pages/Home"));
+const Question_Type = lazy(() => import("./pages/Question_Type"));
+const Text_Input = lazy(() => import("./pages/Text_Input"));
+const Output = lazy(() => import("./pages/Output"));
+const Previous = lazy(() => import("./pages/Previous"));
+const NotFound = lazy(() => import("./pages/PageNotFound"));
 
 function App() {
   return (
     <HashRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/question-type" element={<Question_Type />} />
-        <Route path="/input" element={<Text_Input />} />
-        <Route path="/output" element={<Output />} />
-        <Route path="/history" element={<Previous />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Suspense 
+        fallback={
+          <div className="w-screen h-screen flex justify-center items-center bg-[#02000F]">
+            <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-[#00CBE7]"></div>
+          </div>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/question-type" element={<Question_Type />} />
+          <Route path="/input" element={<Text_Input />} />
+          <Route path="/output" element={<Output />} />
+          <Route path="/history" element={<Previous />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </HashRouter>
   );
 }
